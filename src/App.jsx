939540eac7da5396078cdb2d9dc476adade869a7
Aakash Kumar Suman <121715040+iamaakashks@users.noreply.github.com>
2025-03-22 19:42:56 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import MovieCard from "./components/MovieCard.jsx";
+import Home from './components/Home.jsx';
+export default function App(){
+  return (
+    <div className="w-full min-h-screen px-[10%] pt-[2%]">
+        <Home />
+    </div>
+  )
+}
---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+export default function MovieCard({movie}){
+    console.log()
+    function handleFavouriteBtn(){
+        console.log("movie bookmarked");
+    }
+    return (
+        <div className="movie_card">
+            <div className="movie_poster w-52 h-72 relative">
+                <img className="w-full h-full object-cover" src={movie.img_link} alt="img" />
+                <button className="absolute top-2 right-2 border-[1px] rounded-full p-1 bg-zinc-800" onClick={handleFavouriteBtn}>❤️</button>
+            </div>
+            <div className="movie_info font-medium">
+                <h3>Title : {movie.title}</h3>
+                <h4>Releasing : {movie.release_date}</h4>
+            </div>
+        </div>
+    )
+}
